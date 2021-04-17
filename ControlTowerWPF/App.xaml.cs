aacfb29734b210a242aa3587eed3dd1f08e23cbf
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-17 11:28:09 +0200

    Implemented structure for DI. Only added MainWindow to services at the moment

--- ControlTowerWPF/App.xaml.cs
+++ ControlTowerWPF/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using AppFeatures;
 
 namespace ControlTowerWPF
 {
@@ -13,5 +15,25 @@ namespace ControlTowerWPF
     /// </summary>
     public partial class App : Application
     {
+        private ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<FlightLogger>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
