2f65ffb0aa928d82ba4d0b623cfbe3d8dbf55548
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-18 17:11:52 +0200

    New file with partial class containing file paths used when doing unit tests

---
+++ DataAccess/FilePathsForTesting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Class containing file paths that used within the app.
+    /// </summary>
+    public static partial class FilePaths
+    {
+        public static string DataStorageRootFolderPathForTesting
+        {
+            get
+            {
+                return Path.GetFullPath(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\DataAccess\Storage\"));
+            }
+        }
+
+        public static string FlightLogsDataFolderPathForTesting
+        {
+            get
+            {
+                return Path.GetFullPath(
+                    Path.Combine(DataStorageRootFolderPathForTesting, @".\FlightLogs\"));
+            }
+        }
+
+        public static string FlightLogFilePathForTesting
+        {
+            get
+            {
+                return Path.GetFullPath(
+                    Path.Combine(FlightLogsDataFolderPathForTesting, @".\FlightLog.xml"));
+            }
+        }
+    }
+}
