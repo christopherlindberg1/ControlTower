2c66bcea232212c04f4eaf3b25eefa9a239c9a4e
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-19 13:58:29 +0200

    Initial commit. Project for testing AppFeatures

---
+++ AppFeatures.Tests/FlightLoggerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppFeatures;
+using DataAccess;
+using Xunit;
+
+namespace AppFeatures.Tests
+{
+    public class FlightLoggerTests
+    {
+        private static FlightLogger _flightLogger = new FlightLogger(FilePaths.FilePathForXmlTestFile);
+    }
+}
