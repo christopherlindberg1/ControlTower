5487a77d73b3df98e1c44209216138028190f2b9
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-19 10:19:04 +0200

    Initial commit

---
+++ AppFeature.Tests/FlightLoggerTestsDataGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppFeatures.Models;
+
+namespace AppFeature.Tests
+{
+    public class FlightLoggerTestsDataGenerator
+    {
+        public static IEnumerable<object> GetSampleFlightLogInfoData()
+        {
+            yield return new FlightLogInfo
+            {
+                FlightCode = "SAS 794",
+                Status = "Took off",
+                DateTime = new DateTime(2020, 5, 23, 14, 52, 15)
+            };
+        }
+    }
+}
