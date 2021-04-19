ef4156da57bc73e80b16ce2ee85b7af3c2245cc5
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-19 11:33:31 +0200

    Initial commit. Class for testing ControlTower functionality

---
+++ ControlTowerWPF.Tests/ControlTowerWindowTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ControlTowerWPF;
+
+namespace ControlTowerWPF.Tests
+{
+    public class ControlTowerWindowTests
+    {
+
+    }
+}
