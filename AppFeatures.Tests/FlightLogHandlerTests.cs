493005573dcd257cf1e7a6daf25c333764cbf046
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-20 12:39:53 +0200

    Added test for filtering flight log with null or empty strings

--- AppFeatures.Tests/FlightLogHandlerTests.cs
+++ AppFeatures.Tests/FlightLogHandlerTests.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppFeatures;
+using AppFeatures.Models;
+using Xunit;
 
 namespace AppFeatures.Tests
 {
     public class FlightLogHandlerTests
     {
+        [Fact]
+        public void FilterFlightLog_NoSearchParameters_ReturnsFullList()
+        {
+            // Arrange
+            FlightLogHandler flightLogHandler = new FlightLogHandler();
+            List<FlightLogInfo> flightLog = FlightLogHandlerTestData.SampleFlightLog;
+            int nrOfItemsInTotal = flightLog.Count;
 
+            // Act
+            List<FlightLogInfo> filteredFlightLog = flightLogHandler.FilterFlightLog(
+                flightLog, "", null, null);
+
+            // Assert
+            Assert.Equal(flightLog.Count, filteredFlightLog.Count);
+        }
     }
 }
