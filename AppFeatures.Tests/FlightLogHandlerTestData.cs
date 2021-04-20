01545a50cf3de5796d9ee3e8f04c1f281a88589c
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-20 12:25:43 +0200

    Class has a sample FlightLogInfo object as well as a collection of those

---
+++ AppFeatures.Tests/FlightLogHandlerTestData.cs
@@ -0,0 +1,34 @@
+﻿using AppFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFeatures.Tests
+{
+    public class FlightLogHandlerTestData
+    {
+        private static FlightLogInfo _sampleFlightLogInfoItem = new FlightLogInfo
+        {
+            FlightCode = "SAS 333",
+            Status = "Landed",
+            DateTime = new DateTime(2021, 4, 20, 14, 15, 15)
+        };
+
+        private static List<FlightLogInfo> _sampleFlightLog = new List<FlightLogInfo>
+        {
+            new FlightLogInfo
+            {
+                FlightCode = "SAS 794",
+                Status = "Took off",
+                DateTime = new DateTime(2020, 1, 1, 12, 45, 45)
+            },
+            // Add more items...
+        };
+
+        public static List<FlightLogInfo> SampleFlightLog { get => _sampleFlightLog; }
+
+        public static FlightLogInfo SampleFlightLogInfoItem { get => _sampleFlightLogInfoItem; }
+    }
+}
