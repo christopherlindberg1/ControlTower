67ba5baef0e0c70a6f7ee8db7de829231c848cda
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-22 21:58:44 +0200

    Initial commit. Class that implements ITextFileFlightLogger

---
+++ DataAccess/XmlFlightLogger.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class XmlFlightLogger : ITextFileFlightLogger
+    {
+        public List<FlightLogInfo> GetLog(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveEntryInLog(string filePath, FlightLogInfo objectToSave)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
