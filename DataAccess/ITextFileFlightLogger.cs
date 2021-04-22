876e8055891c9b8fa593b492c572e7a59eb9b904
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-23 00:22:59 +0200

    Removed property FlightLog since I don't want this class to store the log in RAM

--- DataAccess/ITextFileFlightLogger.cs
+++ DataAccess/ITextFileFlightLogger.cs
@@ -12,8 +12,6 @@ namespace DataAccess
     {
         string FilePath { get; }
 
-        List<FlightLogInfo> FlightLog { get; }
-
         List<FlightLogInfo> GetLog();
 
         void SaveEntryInLog(FlightLogInfo flightLogEntry);
