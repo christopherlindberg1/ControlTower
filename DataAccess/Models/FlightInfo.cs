29429f43e1597654d369b930b7e13b033e2cbb62
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-22 22:01:06 +0200

    Moved models to the DataAccess project

---
+++ DataAccess/Models/FlightInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    /// <summary>
+    /// Model used to store flight data that will be used in the ListView in the main window.
+    /// </summary>
+    public class FlightInfo
+    {
+        public string FlightCode { get; set; }
+        public string Status { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        public string DisplayDateTime
+        {
+            get => $"{ DateTime.ToLongTimeString() } - { DateTime.ToShortDateString() }";
+        }
+    }
+}
