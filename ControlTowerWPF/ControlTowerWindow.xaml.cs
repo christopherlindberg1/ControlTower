3e88c6a530594391e4aafc719d9d6fa7c0b0d1f7
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-22 22:10:18 +0200

    Changed using statement after having moved the models to the DataAccess project

--- ControlTowerWPF/ControlTowerWindow.xaml.cs
+++ ControlTowerWPF/ControlTowerWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
 using DataAccess;
-using AppFeatures.Models;
+using DataAccess.Models;
 using System.Text.RegularExpressions;
 
 namespace ControlTowerWPF
