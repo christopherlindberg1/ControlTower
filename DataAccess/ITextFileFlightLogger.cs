71d6f922494242523a4ee7c51ef106d94f73152e
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-22 21:55:53 +0200

    Renamed interface. Changed methods to be specific for flightlog data and not gave generic parameters

---
+++ DataAccess/ITextFileFlightLogger.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccess
+{
+    interface ITextFileFlightLogger
+    {
+        List<FlightLogInfo> GetLog(string filePath);
+
+        void SaveEntryInLog(string filePath, FlightLogInfo objectToSave);
+    }
+}
