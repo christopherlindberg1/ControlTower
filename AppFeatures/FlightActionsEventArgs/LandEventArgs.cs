db9c854ee661c37a7f19a06c5e9a7b450fd164a9
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-23 12:26:38 +0200

    Moved event arg-classes to separate folder

---
+++ AppFeatures/FlightActionsEventArgs/LandEventArgs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFeatures.FlightActionsEventArgs
+{
+    /// <summary>
+    /// Manages data that is of interest for when an airplane lands.
+    /// </summary>
+    public class LandEventArgs : EventArgs 
+    {
+        private string _flightCode;
+
+
+        /// <summary>
+        /// Flight code of the airplane
+        /// </summary>
+        public string FlightCode
+        {
+            get => _flightCode;
+
+            set => _flightCode = value ??
+                throw new ArgumentNullException("FlightCode", "FlightCode cannot be null");
+        }
+
+        /// <summary>
+        /// Readonly property for status, since the status is always the same when
+        /// the landing event occurs.
+        /// </summary>
+        public string Status { get; } = "Landed";
+
+        public DateTime DateTime { get; } = DateTime.Now;
+    }
+}
