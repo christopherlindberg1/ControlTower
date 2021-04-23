35560b7c88d79bd38e27ba43f02677aca8531f40
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-23 11:55:08 +0200

    Created interface for a flight log handler

---
+++ AppFeatures/IFlightLogHandler.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFeatures
+{
+    public interface IFlightLogHandler
+    {
+        IFlightLogger FlightLogger { get; }
+
+        List<FlightLogInfo> FlightLogInfoItems { get; }
+
+        int TotalNumberOfFlightLogRecords { get; }
+
+        void AddFlightLogInfoItemToList(FlightLogInfo flightLogInfo);
+
+        void AddFlightLogInfoItemToLog(FlightLogInfo flightLogInfo);
+
+        void OnTakeOff(object source, TakeOffEventArgs e);
+
+        void OnLanded(object source, LandEventArgs e);
+    }
+}
