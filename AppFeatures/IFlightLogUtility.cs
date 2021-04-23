95db788e2fd9398eed5f9ae43a4692ea7cf496a9
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-23 11:22:59 +0200

    Initial commit. Interface for a flight logger utility class

---
+++ AppFeatures/IFlightLogUtility.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFeatures
+{
+    public interface IFlightLogUtility
+    {
+        List<FlightLogInfo> FilterFlightLog(
+            List<FlightLogInfo> flightLogInfoItems,
+            string searchTerm,
+            DateTime? startDate,
+            DateTime? endDate);
+    }
+}
