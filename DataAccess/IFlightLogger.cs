d1948faca2476ae1d592db37d6ac9b4a63124d5c
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-23 11:41:15 +0200

    Moved the most general methods from ITextFileFlightLogger to this file. (GetLog() and SaveEntryInLog())

--- DataAccess/IFlightLogger.cs
+++ DataAccess/IFlightLogger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace DataAccess
 {
     public interface IFlightLogger
     {
+        List<FlightLogInfo> GetLog();
+
+        void SaveEntryInLog(FlightLogInfo flightLogEntry);
     }
 }
