775c4e5a30b5fc66943fd9912b7f41a1bc1c02cf
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-26 12:33:57 +0200

    Created class for generating an airline image that corresponds to an airline code. Logic was in UI before

---
+++ AppFeatures/AirlineImageGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFeatures
+{
+    public class AirlineImageGenerator
+    {
+        public Uri GetImageUri(string flightCode)
+        {
+            Uri pathToImage = new Uri("/Images/Airline-unknown.jpg", UriKind.Relative);
+
+            if (flightCode.ToLower().Contains("dlh"))
+            {
+                pathToImage = new Uri("/Images/Airline-dlh.jpg", UriKind.Relative);
+            }
+            else if (flightCode.ToLower().Contains("rys"))
+            {
+                pathToImage = new Uri("/Images/Airline-rys.jpg", UriKind.Relative);
+            }
+            else if (flightCode.ToLower().Contains("sas"))
+            {
+                pathToImage = new Uri("/Images/Airline-sas.jpg", UriKind.Relative);
+            }
+
+            return pathToImage;
+        }
+    }
+}
