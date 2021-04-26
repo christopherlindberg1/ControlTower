44ee38daf09ddef60b84b02498cb4d9552ab1f07
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-04-26 14:07:18 +0200

    Gave the correct Uri's  to the images

--- AppFeatures/AirlineImageGenerator.cs
+++ AppFeatures/AirlineImageGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,19 +11,19 @@ namespace AppFeatures
     {
         public Uri GetImageUri(string flightCode)
         {
-            Uri pathToImage = new Uri("/Images/Airline-unknown.jpg", UriKind.Relative);
+            Uri pathToImage = new Uri($"{ FilePaths.ImagesRootFilder }Airline-unknown.jpg", UriKind.Absolute);
 
             if (flightCode.ToLower().Contains("dlh"))
             {
-                pathToImage = new Uri("/Images/Airline-dlh.jpg", UriKind.Relative);
+                pathToImage = new Uri($"{ FilePaths.ImagesRootFilder }Airline-dlh.jpg", UriKind.Absolute);
             }
             else if (flightCode.ToLower().Contains("rys"))
             {
-                pathToImage = new Uri("/Images/Airline-rys.jpg", UriKind.Relative);
+                pathToImage = new Uri($"{ FilePaths.ImagesRootFilder }Airline-rys.jpg", UriKind.Absolute);
             }
             else if (flightCode.ToLower().Contains("sas"))
             {
-                pathToImage = new Uri("/Images/Airline-sas.jpg", UriKind.Relative);
+                pathToImage = new Uri($"{ FilePaths.ImagesRootFilder }Airline-sas.jpg", UriKind.Absolute);
             }
 
             return pathToImage;
